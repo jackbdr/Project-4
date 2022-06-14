ec6ad3bb2d9ac71600f9b8d1f94dcfc4e26f4e58
Jack <jackbdrobinson@gmail.com>
2022-06-14 09:32:37 +0100

    popups

--- client/src/components/animals/AnimalDetail.js
+++ client/src/components/animals/AnimalDetail.js
@@ -24,106 +24,124 @@ const AnimalDetail = () => {
     getAnimal()
   }, [id])
 
+  const handlePage = (e) => {
+    e.preventDefault()
+    if (e.target.className === 'arrow-left' && page === 1) return
+    if (e.target.className === 'arrow-left' && page === 2) {
+      setPage(1)
+    }
+    if (e.target.className === 'arrow-right' && page === 2) return
+    if (e.target.className === 'arrow-right' && page === 1) {
+      setPage(2)
+    }
+  }
+
   return (
     <>{animal ?
       page === 1 ?
         <section className='page1'>
           <section className='page1-animal'>
-            <img src={animal.img_1} />
-            <div className='animal-info'>
-              <div className='animal-header'>
-                <div className='an-header-left'>
-                  <h1>{animal.name}</h1>
-                  <h2>{animal.sci_name}</h2>
-                  <p>{animal.an_group}</p>
+            <div className='animal-wrapper'>
+              <img className='img-1' src={animal.img_1} />
+              <div className='animal-info'>
+                <div className='animal-header'>
+                  <div className='an-header-left'>
+                    <h1>{animal.name}</h1>
+                    <h2>{animal.sci_name}</h2>
+                    <p>{animal.an_group}</p>
+                  </div>
+                  <div className='an-header-right'>
+                    <p>rating/comment/favourite</p>
+                  </div>
                 </div>
-                <div className='an-header-right'>
-                  <p>rating/comment/favourite</p>
+                <p className='description'>{animal.description}</p>
+                <div className='icon-info'>
+                  {animal.height || animal.length &&
+                    <div className='size'>
+                      <img className='icon' src='/images/ruler.png' alt='ruler' />
+                      {animal.height &&
+                        <p>{animal.height} (height)</p>
+                      }
+                      {animal.length &&
+                        <p>{animal.length} (length)</p>
+                      }
+                    </div>
+                  }
+                  {animal.avg_weight &&
+                    <div className='weight'>
+                      <img className='icon' src='/images/scales.png' alt='scales' />
+                      <p>~{animal.avg_weight} kg</p>
+                    </div>
+                  }
+                  <div className='habitat'>
+                    <img className='icon' src='/images/home-icon.png' alt='house' />
+                    <p>{animal.habitat}</p>
+                  </div>
+                  <div className='diet'>
+                    <img className='icon' src='/images/knife-fork.png' alt='knife-fork' />
+                    <p>{animal.diet}</p>
+                  </div>
+                  {animal.life_span &&
+                    <div className='lifespan'>
+                      <img className='icon' src='/images/lifespan.png' alt='lifespan-icon' />
+                      <p>{animal.life_span}</p>
+                    </div>
+                  }
+                  {animal.con_status === 1 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/extinct.png' />
+                      <p>Extinct</p>
+                    </div>
+                  }
+                  {animal.con_status === 2 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/extinct-wild.png' />
+                      <p>Extinct in the wild</p>
+                    </div>
+                  }
+                  {animal.con_status === 3 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/critically-endangered.png' />
+                      <p>Critically Endangered</p>
+                    </div>
+                  }
+                  {animal.con_status === 4 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/endangered.png' />
+                      <p>Endangered</p>
+                    </div>
+                  }
+                  {animal.con_status === 5 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/vulnerable.png' />
+                      <p>Vulnerable</p>
+                    </div>
+                  }
+                  {animal.con_status === 6 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/near-threatened.png' />
+                      <p>Near Threatened</p>
+                    </div>
+                  }
+                  {animal.con_status === 7 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/least-concern.png' />
+                      <p>Least Concern</p>
+                    </div>
+                  }
+                  {animal.con_status === 8 &&
+                    <div className='con-status'>
+                      <img className='icon' src='/images/data-def.png' />
+                      <p>Data Deficient</p>
+                    </div>
+                  }
                 </div>
+                <Link to='/map'>View on map</Link>
               </div>
-              <p className='description'>{animal.description}</p>
-              <div className='icon-info'>
-                {animal.height || animal.length &&
-                  <div className='size'>
-                    <img className='icon' src='/images/ruler.png' alt='ruler' />
-                    {animal.height &&
-                      <p>{animal.height} (height)</p>
-                    }
-                    {animal.length &&
-                      <p>{animal.length} (length)</p>
-                    }
-                  </div>
-                }
-                {animal.avg_weight &&
-                  <div className='weight'>
-                    <img className='icon' src='/images/scales.png' alt='scales' />
-                    <p>~{animal.avg_weight} kg</p>
-                  </div>
-                }
-                <div className='habitat'>
-                  <img className='icon' src='/images/home-icon.png' alt='house' />
-                  <p>{animal.habitat}</p>
-                </div>
-                <div className='diet'>
-                  <img className='icon' src='/images/knife-fork.png' alt='knife-fork' />
-                  <p>{animal.diet}</p>
-                </div>
-                {animal.life_span &&
-                  <div className='lifespan'>
-                    <img className='icon' src='/images/lifespan.png' alt='lifespan-icon' />
-                    <p>{animal.life_span}</p>
-                  </div>
-                }
-                {animal.con_status === 1 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/extinct.png' />
-                    <p>Extinct</p>
-                  </div>
-                }
-                {animal.con_status === 2 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/extinct-wild.png' />
-                    <p>Extinct in the wild</p>
-                  </div>
-                }
-                {animal.con_status === 3 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/critically-endangered.png' />
-                    <p>Critically Endangered</p>
-                  </div>
-                }
-                {animal.con_status === 4 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/endangered.png' />
-                    <p>Endangered</p>
-                  </div>
-                }
-                {animal.con_status === 5 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/vulnerable.png' />
-                    <p>Vulnerable</p>
-                  </div>
-                }
-                {animal.con_status === 6 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/near-threatened.png' />
-                    <p>Near Threatened</p>
-                  </div>
-                }
-                {animal.con_status === 7 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/least-concern.png' />
-                    <p>Least Concern</p>
-                  </div>
-                }
-                {animal.con_status === 8 &&
-                  <div className='con-status'>
-                    <img className='icon' src='/images/data-def.png' />
-                    <p>Data Deficient</p>
-                  </div>
-                }
-              </div>
-              <Link to='/map'>View on map</Link>
+            </div>
+            <div className='arrow-buttons'>
+              <button className='arrow-left' onClick={handlePage}></button>
+              <button className='arrow-right' onClick={handlePage}></button>
             </div>
           </section>
           <section className='comments'>
@@ -143,6 +161,10 @@ const AnimalDetail = () => {
         <section className='page2'>
           <h4>{animal.fact}</h4>
           <img src={animal.img_2} />
+          <div className='arrow-buttons'>
+            <button className='arrow-left' onClick={handlePage}></button>
+            <button className='arrow-right' onClick={handlePage}></button>
+          </div>
         </section>
       :
       <h2>{errors && 'Sorry, we had trouble retrieving the data'}</h2>
--- client/src/components/explore/MapPage.js
+++ client/src/components/explore/MapPage.js
@@ -1,10 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
+import axios from 'axios'
+import mapboxgl, { accessToken } from 'mapbox-gl'
+import ReactMapGL, { Marker, Source, Layer, Popup } from 'react-map-gl'
 
 import { MapAncientStyle } from './MapAncientStyle'
 import { MapModernStyle } from './MapModernStyle'
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiamFja2JkciIsImEiOiJjbDQ4azk2djMwMm5qM2NtaWF3YTBiOHRqIn0.B_CRBzLyOuY5KAV0quy-Hg'
+import Pin from '../../images/pinkpin.png'
 
 const DisplayMap = () => {
   const mapContainer = useRef(null)
@@ -12,36 +14,42 @@ const DisplayMap = () => {
   const [lng, setLng] = useState(11.78)
   const [lat, setLat] = useState(28.91)
   const [zoom, setZoom] = useState(1)
-  const [style, setStyle] = useState(MapModernStyle)
+  const [style, setStyle] = useState(MapAncientStyle)
+  const [animals, setAnimals] = useState([])
   const [isAncient, setIsAncient] = useState(true)
 
-  useEffect(() => {
-    if (map.current && style === map.current.style.stylesheet) return // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: style,
-      center: [lng, lat],
-      zoom: zoom,
-      minZoom: 1,
-      attributionControl: false,
-    })
-  }, [style])
+  const accessToken = 'pk.eyJ1IjoiamFja2JkciIsImEiOiJjbDQ4azk2djMwMm5qM2NtaWF3YTBiOHRqIn0.B_CRBzLyOuY5KAV0quy-Hg'
+
 
   useEffect(() => {
-    if (!map.current) return // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4))
-      setLat(map.current.getCenter().lat.toFixed(4))
-      setZoom(map.current.getZoom().toFixed(2))
-    })
-  })
+    const getAnimals = async () => {
+      try {
+        const { data } = await axios.get('/api/animals')
+        setAnimals(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getAnimals()
+  }, [])
 
-  const handleToggle = (e) => {
+  const [popup, setPopup] = useState(null)
+  const [selectedMarker, setSelectedMarker] = useState(null)
+
+  const openPopup = (animal, id) => {
+    setPopup(animal)
+    setSelectedMarker(id)
+  }
+
+  const handleButtons = (e) => {
     e.preventDefault()
-    if (map.current.style.stylesheet === MapAncientStyle) {
-      setStyle(MapModernStyle)
-    } else {
+    if (e.target.className === 'ancient-button' && map.current.style.stylesheet === MapAncientStyle) return
+    if (e.target.className === 'ancient-button' && map.current.style.stylesheet !== MapAncientStyle) {
       setStyle(MapAncientStyle)
+    }
+    if (e.target.className === 'modern-button' && map.current.style.stylesheet === MapModernStyle) return
+    if (e.target.className === 'modern-button' && map.current.style.stylesheet !== MapModernStyle) {
+      setStyle(MapModernStyle)
     }
     // console.log(style)
     console.log(map.current.style.stylesheet)
@@ -49,8 +57,46 @@ const DisplayMap = () => {
 
   return (
     <div>
-      <button onClick={handleToggle}>Toggle map</button>
-      <div ref={mapContainer} className="map-container" />
+      <button className='ancient-button' onClick={handleButtons}>Ancient</button>
+      <button className='modern-button' onClick={handleButtons}>Modern</button>
+      {/* <div ref={mapContainer} className="map-container" /> */}
+      <ReactMapGL
+        mapboxAccessToken={accessToken}
+        initialViewState={{
+          longitude: 11.87,
+          latitude: 28.91,
+          zoom: 1,
+          minZoom: 1,
+        }}
+        mapStyle='mapbox://styles/jackbdr/cl4cypbf6000t15mk3jy6tx18'
+      >
+        {animals.length > 0 &&
+          animals.map(animal => {
+            const { id, long, lat } = animal
+            console.log(lat, long)
+            return (
+              <>
+                <Marker key={id} longitude={long} latitude={lat} scale='0.1'>
+                  <img className='pin' src={Pin} onClick={() => openPopup(animal, id)} />
+                </Marker>
+                {(popup && selectedMarker === id) &&
+                  <Popup
+                    key={id}
+                    longitude={long}
+                    latitude={lat}
+                    onClose={() => setPopup(null)}
+                    closeButton={true}
+                    closeOnClick={false}
+                    offsetTop={-30}
+                  >
+                    <img src={popup.img_1} />
+                  </Popup>
+                }
+              </>
+            )
+          })
+        }
+      </ReactMapGL>
     </div>
   )
 
